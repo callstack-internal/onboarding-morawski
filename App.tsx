e891eb4fc628d90a002d5f6b35db2a5c8544e483
Tom <t.p.morawski@gmail.com>
2023-01-24 15:32:12 +0100

    react init, vector icons, react-navigation

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+// import { WeatherList } from './src/components/WeatherList';
+
+const OPEN_WEATHER_API_KEY = "ba7775fa717190ec593df1d334712a82"
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        {/* <WeatherList /> */}
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+}
+
+export default App;
