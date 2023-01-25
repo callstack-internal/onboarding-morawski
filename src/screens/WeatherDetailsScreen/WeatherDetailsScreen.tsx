9caed59a4c599f5c5b47e0d946593c0d1b80dd7f
Tom <t.p.morawski@gmail.com>
2023-01-25 13:13:18 +0100

    refactor repetition

--- src/screens/WeatherDetailsScreen/WeatherDetailsScreen.tsx
+++ src/screens/WeatherDetailsScreen/WeatherDetailsScreen.tsx
@@ -18,26 +18,35 @@ const WeatherDetailsScreen: React.FC = () => {
     clouds: {all},
   } = item;
 
+  const data = [
+    {
+      key: 'Humidity',
+      value: `${humidity}%`,
+    },
+    {
+      key: 'Pressure',
+      value: `${pressure} hPa`,
+    },
+    {
+      key: 'Wind Speed',
+      value: `${humidity} mPh`,
+    },
+    {
+      key: 'Cloud cover',
+      value: `${humidity}%`,
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <WeatherGeneralInfo item={item} />
       <View style={styles.bottomContainer}>
-        <View style={styles.textContainer}>
-          <Text style={styles.label}>Humidity:</Text>
-          <Text style={styles.value}>{humidity}%</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.label}>Pressure:</Text>
-          <Text style={styles.value}>{pressure} hPa</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.label}>Wind Speed:</Text>
-          <Text style={styles.value}>{speed} mPh</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.label}>Cloud cover:</Text>
-          <Text style={styles.value}>{all}%</Text>
-        </View>
+        {data.map(d => (
+          <View key={d.key} style={styles.textContainer}>
+            <Text style={styles.label}>{d.key}:</Text>
+            <Text style={styles.value}>{d.value}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
