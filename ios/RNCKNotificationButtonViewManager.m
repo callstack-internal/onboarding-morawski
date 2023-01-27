d04d12786b0af94abc4a7b3e8c1e632e1b2c4041
Tom <t.p.morawski@gmail.com>
2023-01-27 14:00:51 +0100

    fix ios

--- ios/RNCKNotificationButtonViewManager.m
+++ ios/RNCKNotificationButtonViewManager.m
@@ -1,6 +1,6 @@
 // RNCKNotificationButtonViewManager.m
 #import "RNCKNotificationButtonViewManager.h"
-#import "RNCKNotificiationButton.h"
+#import "RNCKNotificationButton.h"
 @implementation RNCKNotificationButtonViewManager
 
 // 2.
@@ -9,7 +9,7 @@ RCT_EXPORT_MODULE(NotificiationButton)
 // 3.
 - (UIButton *)view
 {
-    return [[RNCKNotificiationButton alloc] initWithFrame:CGRectZero];
+    return [[RNCKNotificationButton alloc] initWithFrame:CGRectZero];
 }
 
 @end
